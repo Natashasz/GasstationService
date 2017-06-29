2a632f333783a506495a165ec7511536e5aae293
Alexander Brauckmann <a@a.com>
2017-06-29 14:46:11 +0200

    GET paritally implemented.

--- nodejs-server-server/controllers/DefaultService.js
+++ nodejs-server-server/controllers/DefaultService.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var request = require('request');
+
+var apiKey = 'e2be84a8-d1d1-3e1d-7619-0c354b0b267c';
+
 exports.stationStation_idGET = function(args, res, next) {
   /**
    * Get price trend for a given gas station.
@@ -32,27 +36,75 @@ exports.stationsGET = function(args, res, next) {
    * radius BigDecimal  (optional)
    * returns List
    **/
-  var examples = {};
-  examples['application/json'] = [ {
-  "address" : "aeiou",
-  "name" : "aeiou",
-  "opening hours" : [ {
-    "start" : "aeiou",
-    "end" : "aeiou",
-    "text" : "aeiou"
-  } ],
-  "prices" : [ {
-    "gastype" : "aeiou",
-    "price" : 1.3579000000000001069366817318950779736042022705078125
-  } ],
-  "brand" : "aeiou"
-} ];
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
+
+  // (1) Parse Parameters
+  var latitude = args.latitude.originalValue
+  var longitude = args.longitude.originalValue;
+  var radius = args.radius.originalValue;
+
+  // (2) Build URL for TanskerkoenigAPI request
+  var query = 'https://creativecommons.tankerkoenig.de/json/list.php?'
+              + 'lat=' + latitude
+              + '&lng=' + longitude
+              + '&rad=' + radius
+              + '&sort=dist'
+              + '&type=all'
+              + '&apikey=' + apiKey;
+  console.log("Query: " + query);
+
+  request(query, function (error, response, body) {
+      var bodyParsed = JSON.parse(body);
+
+      var gasStationsReturn = [];
+      for(var gasStationIdx in bodyParsed.stations) {
+        var gasStation = bodyParsed.stations[gasStationIdx];
+
+        var g = {};
+
+        // Address
+        g.address = gasStation.street;
+
+        // Name
+        g.name = gasStation.name;
+
+        // Opening Hours
+        var detailQuery = 'https://creativecommons.tankerkoenig.de/json/detail.php?'
+                          + 'id=' + gasStation.id
+                          + '&apikey=' + apiKey;
+        request(detailQuery, function (detailError, detailResponse, detailBody) {
+          console.log(detailBody);
+        });
+
+        // Prices
+        g.prices = [];
+        g.prices.push({
+            'gastype':'diesel',
+            'price':gasStation.diesel
+        });
+        g.prices.push({
+            'gastype':'e5',
+            'price':gasStation.e5
+        });
+        g.prices.push({
+            'gastype':'e10',
+            'price':gasStation.e10
+        });
+
+        // Brand
+        g.brand = gasStation.brand;
+
+        gasStationsReturn.push(g);
+      }
+
+      var examples = {};
+      examples['application/json'] = gasStationsReturn;
+      if (Object.keys(examples).length > 0) {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+      } else {
+          res.end();
+      }
+  });
 }
 
 exports.stationsSnapshotPOST = function(args, res, next) {
